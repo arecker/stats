cb344cef5df07cc18603241e80f1d7b67ccdb229
Alex Recker <alex@reckerfamily.com>
2021-05-06 00:56:38 -0500

    Build all, progress bar

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module alexrecker.com/stats
 
 go 1.13
+
+require github.com/schollz/progressbar/v3 v3.8.0
--- src/main.go
+++ src/main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
+	"path"
 	"sort"
 	"sync"
+
+	progressbar "github.com/schollz/progressbar/v3"
 )
 
-func processEntry(entry *Entry, wg *sync.WaitGroup) {
+func processEntry(entry *Entry, wg *sync.WaitGroup, progressBar *progressbar.ProgressBar) {
 	defer wg.Done()
 
 	// read in words
@@ -21,22 +26,58 @@ func processEntry(entry *Entry, wg *sync.WaitGroup) {
 	sort.Strings(words)
 	data, err := json.MarshalIndent(words, "", "  ")
 	if err != nil {
-		log.Fatal(error)
+		log.Fatal(err)
 	}
 
 	// write to file
 	if error = ioutil.WriteFile(entry.WordsFilePath, data, 0644); error != nil {
 		log.Fatal(error)
 	}
+
+	progressBar.Add(1)
 }
 
 func main() {
 	var wg sync.WaitGroup
+	entries := Entries()
+	progressBar := progressbar.Default(int64(len(entries)), "Scanning entries")
 
 	for _, entry := range Entries() {
 		wg.Add(1)
-		go processEntry(entry, &wg)
+		go processEntry(entry, &wg, progressBar)
 	}
 
 	wg.Wait()
+
+	progressBar = progressbar.Default(int64(len(entries)), "Compiling total words")
+	var allWords []string
+	for _, entry := range entries {
+		reader, error := os.Open(entry.WordsFilePath)
+		if error != nil {
+			log.Fatal(error)
+		}
+
+		byteValue, error := ioutil.ReadAll(reader)
+		if error != nil {
+			log.Fatal(error)
+		}
+
+		var theseWords []string
+		json.Unmarshal(byteValue, &theseWords)
+		allWords = append(allWords, theseWords...)
+		progressBar.Add(1)
+	}
+
+	fmt.Printf("Building all words (%d)\n", len(allWords))
+	sort.Strings(allWords)
+	data, error := json.MarshalIndent(allWords, "", "  ")
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	if error = ioutil.WriteFile(path.Join(WordsDir, "all.json"), data, 0644); error != nil {
+		log.Fatal(error)
+	}
+
+	fmt.Println("Done!")
 }
